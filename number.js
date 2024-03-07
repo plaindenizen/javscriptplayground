882ffaafb206de6acb8f3f04ce5b35c22c320af3
jacob <160391360+plaindenizen@users.noreply.github.com>
2024-03-07 14:26:46 +1300

    Add files via upload

---
+++ number.js
@@ -0,0 +1,19 @@
+//NOTE - 
+/*
+const, let and var are commands that declare variables. They have different effects. Let variables are allowed to be changed. Const variables are not allowed to be changed.
+Const and let are more commonly used nowadays.
+Its a good idea to stick to one for consitency sake Variables containing integers can be added together for basic addition.
+When troubleshooting errors in code, going through and highlight them with note function can help.
+In Javascript, anything inbetween two "()" brackets is considered a 'method'
+*/
+const pi = 3.14
+//pi= 10 (runing this line will display an error message)
+console.log(pi)
+
+let num2 = 20
+//num2= 25 (running this line is allowed) if no number is defined to let variable it will simply output 'undefined'
+console.log(num2)
+
+var num3 = 30
+console.log(num3)
+//
