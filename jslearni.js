81a895854234cf0d1818505874f0adf745524e1f
Jacob Barber <jakenbake12@live.com>
2024-04-16 13:20:09 +1200

    basic functions(objects,parameters), basic strings &  concatentation, basic arrays(.length, calling array elements), readline module.

--- jslearni.js
+++ jslearni.js
@@ -25,9 +25,9 @@
 
 // console.log(z)
 
-x = 10, y = 6;
-x += y;     //x is now 16
-console.log(x)
+// x = 10, y = 6;
+// x += y;     //x is now 16
+// console.log(x)
 
 //the += (addition assignment operator) performs addition on the two operands and assigns the result to the left operand.
 //other math can be done using multiplication assignment operator, division assignment operator, subtraction assignment operator.
@@ -41,10 +41,89 @@ console.log(x)
 // };                          //this data type is an object
 
 
-//executing a function can be as simple as simply calling for it on its own like so
+//executing a function (that has already been declared) can be as simple as simply calling for it on its own like so
 
-function myFunction() {
-    console.log("Hello world");
-}
+// function myFunction() {
+//     console.log("Hello world");
+// }
 
-myFunction()    //this prints "Hello World" in the terminal
+// myFunction()    //this prints "Hello World" in the terminal
+
+// function myFunction() {     //anything contained within the smooth brackets after the name of the function is a parameter
+//     return "Hello";         //anything contained within the curly brackets after a function is an object
+// }
+
+// myFunction()    //unlike previous function this one is missing console log in the function and so wont print anything unless specified outside of the function
+// console.log(myFunction())   //this line prints "Hello" in terminal
+
+
+// function myFunction(world = "World") {
+//     return " Hello" + world;
+// }   //statements contained within a function may call upon the parameter of their function.
+
+// console.log(myFunction())
+
+
+// const person = {
+//     myName:     "Jacob",
+//     lastName:   "Barber"
+// };
+
+// console.log(person.myName,person.lastName)
+
+// let s1 = "going to ";
+// let s2 = "the store ";
+// let s3 = "my house ";
+
+// a = s1+s2   
+// b = s1+s3   //here is an example of cancatenation.
+
+// console.log(a)
+// console.log(b)
+
+// let s1 = "going to ";
+// let s2 = "the store ";
+// let s3 = "my house ";
+
+// console.log(s1.concat(s2))  //here is an example of cancatenation.
+
+// sent = "I went boxing a zebra seaside fairy heights"
+
+// console.log(sent.toUpperCase())     //toUpperCase method converts all character sin a string to capitals. toLowerCase converts to lower case.
+
+// const para = "I was walking until I died";
+// console.log(para.replace("until", "because"));      //replace function creates a new string by takeing a specified piece of a string and replaces it with more specified text.
+
+// console.log(para.toUpperCase())                     //another toUpperCase use example
+
+
+//SECTION - Arrays
+
+// const myItems = ['car', 'xylophone', 'GPS']
+// myItems[0] = 'truck'
+
+// console.log(myItems)
+
+// console.log(myItems.length)     //length data property represents the number of elements in a given array
+
+// myItems.length = 5;     //length can be invoked to modify the length of an array. In this case it simply adds two empty items.
+
+// console.log(myItems)
+
+//ANCHOR - Prompt test
+// let sign = prompt("who are you?");
+
+const readline = require('readline')
+
+const r1 = readline.Interface({     //NOTE - 'require' is a global method that allows us to import external modules into nodeJS.
+    input: process.stdin,
+    output: process.stdout
+})
+
+r1.question('whats your name?', (answer) => {
+    console.log(answer.concat(' is your name'))
+    process.exit()      //if process.exit is left out user is stuck in terminal
+}) 
+
+// sequence2 = 'after breakpoint string'
+// console.log(sequence2)
